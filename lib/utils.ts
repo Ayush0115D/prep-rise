59f6d31b0914f650d2b25b4a316ca0717739e7d2
Ayush0115D <dhakreayush578@gmail.com>
2025-05-31 19:04:11 +0530

    added few updations

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 const techIconBaseURL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons";
-
 const normalizeTechName = (tech: string) => {
   const key = tech.toLowerCase().replace(/\.js$/, "").replace(/\s+/g, "");
   return mappings[key as keyof typeof mappings];
